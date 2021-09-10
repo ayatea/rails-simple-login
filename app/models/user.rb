5c736598d0c0b3dcc77bbc173bc40a167b884f73
Ayatea <ayatea1223@gmail.com>
2021-09-11 00:11:46 +0900

    :+1: [#3] スキーマインフォの出力を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  deleted_at      :datetime
+#  hashed_password :string           not null
+#  name            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  login_id        :string           not null
+#
 class User < ApplicationRecord
   acts_as_paranoid
 end
