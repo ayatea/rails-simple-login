b63f70952f2e7c24db23d2c6e1e23546e9b90fc5
Ayatea <ayatea1223@gmail.com>
2021-10-01 20:31:17 +0900

    :+1: [#7] Sessionsコントローラーのリクエストテストを追加

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Sessions', type: :request do
+  describe "GET /new" do
+    it 'レスポンスステータス200が取得されること' do
+      get root_path
+      expect(response).to have_http_status :ok
+    end
+
+    it 'new(ログイン画面)テンプレートが表示されること' do
+      get root_path
+      expect(response).to render_template :new
+    end
   end
 end
