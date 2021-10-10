dd3f58a0e1c84411d8f728577b0b369f47ef55a2
Ayatea <ayatea1223@gmail.com>
2021-10-11 00:16:54 +0900

    :sparkles: [#9] ユーザー認証機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,29 @@ class SessionsController < BaseController
       render action: 'new'
     end
   end
+
+  def create
+    @form = LoginForm.new(login_params)
+    user = User.find_by(login_id: @form.login_id)
+
+    if UserAuthenticateService.new(user).authenticate(@form.password)
+      # ユーザー認証成功の処理
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      # ユーザー認証失敗の処理
+      flash[:danger] = 'ユーザーの認証に失敗しました。'
+      redirect_to action: 'new'
+    end
+  end
+
+  private
+
+  # ログインフォームのストロングパラメーター
+  def login_params
+    params.require(:login_form).permit(
+      :login_id,
+      :password
+    )
+  end
 end
