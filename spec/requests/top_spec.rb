4aca81d8a98be958f2e6ae676a45d01ae6f55e72
Ayatea <ayatea1223@gmail.com>
2021-10-11 22:26:08 +0900

    :green_heart: [#9] Top画面のリクエストテストにログイン機能を反映

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Top', type: :request do
+  include AuthenticationHelpers
+
+  let(:user) { create(:user) }
+
   describe 'GET /index' do
-    it 'レスポンスステータス200が取得されること' do
-      get root_path
-      expect(response).to have_http_status :ok
+    describe 'ログイン済みの場合' do
+      it 'レスポンスステータス200が取得されること' do
+        login(user, 'password')
+        get root_path
+        expect(response).to have_http_status :ok
+      end
+
+      it 'index(トップ画面)テンプレートが表示されること' do
+        login(user, 'password')
+        get root_path
+        expect(response).to render_template :index
+      end
     end
 
-    it 'index(Welcome画面)テンプレートが表示されること' do
-      pending
-      get root_path
-      expect(response).to render_template :index
+    describe '未ログインの場合' do
+      it 'レスポンスステータス302が取得されること' do
+        get root_path
+        expect(response).to have_http_status :found
+      end
+
+      it 'ログイン画面へリダイレクトされること' do
+        get root_path
+        expect(response).to redirect_to :login
+      end
     end
   end
 end
